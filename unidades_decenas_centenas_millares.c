7c3bc8712d5d0c68d22998a54c03f249a0e9762a
alejandronava <alejandronava@hotmail.com>
2015-11-04 22:05:23 -0600

    alejandronava

---
+++ unidades_decenas_centenas_millares.c
@@ -0,0 +1,35 @@
+#include<stdio.h>
+
+int main(){
+int r,a,b,resultado,resultado2,resultado3,resultado4,resultado5;
+b=10;
+printf("PROGRAMA QUE DETERMINA CUANTAS UNIDADES,DECENAS,CENTENAS Y MILLARES HAY EN EL NUMERO A DIVIDIR\n");
+printf("\nDame una cantidad de 4 cifras\n");
+scanf("%d",&a);
+printf("\nInserta el numero 10\n");
+scanf("%d",&b);
+resultado=a/10;
+
+printf("\nEste es el resultado de cuantas unidades,decenas,centenas y millares hay en el numero a dividir\n");
+r=a%b;
+printf("\nLas unidades son: %d\n",r);
+
+resultado2=resultado/10;
+
+r=resultado%10;
+printf("\nLas decenas son: %d\n",r);
+resultado3=resultado2/10;
+    
+r=resultado2%10;
+printf("\nLas centenas son: %d\n",r);
+resultado4=resultado3/10;
+
+r=resultado3%10;
+printf("\nLos millares son: %d\n",r);
+return 0;
+resultado5=resultado4/10;
+	printf("\nLa divicion es: %d\n",resultado5);
+r=resultado4%10;
+printf("\nLos millares son: %d\n",r);
+
+}
